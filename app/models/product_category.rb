c4beeb9b4c6e5537c2ceb2e713e75b2f7acbc58f
Marco Rojo <mrmarcondes@gmail.com>
2020-10-07 18:24:09 -0300

    fix rubocop

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ProductCategory < ApplicationRecord
-
-  has_many :product_categories, class_name: 'ProductCategory', inverse_of: :product_category
-  belongs_to :product_category, class_name: 'ProductCategory', inverse_of: :product_categories, optional: true
+  has_many :product_categories, class_name: 'ProductCategory', inverse_of: :product_category,
+                                dependent: :destroy
+  belongs_to :product_category, class_name: 'ProductCategory', inverse_of: :product_categories,
+                                optional: true
 end
